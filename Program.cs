dd46faff089e2196baad87d9ddae5ae049fdf857
malpaniriya <riyamalpani75@gmail.com>
2023-08-29 17:32:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudwithCollectionsStudent
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int no = 0;
+            StudentCRUD crud = new StudentCRUD();
+            do
+            {
+                Console.WriteLine("++++++++++++++++++++++++++++++++");
+                Console.WriteLine("1. Student List");
+                Console.WriteLine("2. Get Student by Id");
+                Console.WriteLine("3.Add Student");
+                Console.WriteLine("4.Update Student");
+                Console.WriteLine("5.Delete Student");
+                Console.WriteLine("Select the option");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        List<Student> list = crud.GetStudents();
+                        Console.WriteLine("RollNo \t Name \t Percentage \t CollegeName \t");
+                        foreach (Student s in list)
+                        {
+                            Console.WriteLine($"{s.RollNo}{s.Name}{s.Percentage}{s.CollegeName}");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Student RollNo");
+                        int rollno = Convert.ToInt32(Console.ReadLine());
+                        Student s1 = crud.GetStudentByRollNo(rollno);
+                        Console.WriteLine("RollNo \t Name \t Percentage \t CollegeName \t");
+                        Console.WriteLine($"{s1.RollNo}{s1.Name}{s1.Percentage}{s1.CollegeName}");
+                        break;
+                    case 3:
+                        Student s2 = new Student();
+                        Console.WriteLine("Enter Student RollNo");
+                        s2.RollNo = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Student Name");
+                        s2.Name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Enter Student Percentage");
+                        s2.Percentage = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter CollegeName");
+                        s2.CollegeName = Convert.ToString(Console.ReadLine());
+                        crud.AddStudent(s2);
+                        Console.WriteLine("Data saved Sucessfully...");
+                        break;
+                    case 4:
+                        Student s3 = new Student();
+                        Console.WriteLine("Enter Student RollNo");
+                        s3.RollNo = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Student Name");
+                        s3.Name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Enter Student Percentage");
+                        s3.Percentage = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter CollegeName");
+                        s3.CollegeName = Convert.ToString(Console.ReadLine());
+                        crud.AddStudent(s3);
+                        Console.WriteLine("Data saved Sucessfully...");
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter Student RollNo");
+                        int rollno2 = Convert.ToInt32(Console.ReadLine());
+                        crud.DeleteStudent(rollno2);
+                        Console.WriteLine($"{rollno2} student deleted...");
+                        break;
+                }
+                Console.WriteLine("Press 1 to Continue");
+                no = Convert.ToInt32(Console.ReadLine());
+            }
+            while (no == 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
+
---
+++ Student.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudwithCollectionsStudent
+{
+    public class Student
+    {
+        
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+
+        public double Percentage { get; set; }
+
+        public string CollegeName { get; set; }
+    }
+    public class StudentCRUD
+    {
+        private List<Student> studentlist;
+        public StudentCRUD()
+        {
+            studentlist = new List<Student>()
+            {
+                new Student {RollNo=01,Name="Riya Malpani",Percentage=91,CollegeName="NMIMS"},
+                new Student { RollNo = 02, Name = " Yash Shah", Percentage = 80, CollegeName = "FC" }
+            };
+        }
+        public List<Student> GetStudents()
+        {
+            return studentlist;
+        }
+        public Student GetStudentByRollNo(int rollNo)
+        {
+            Student student = new Student();
+            foreach (Student s in studentlist)
+            {
+                if (s.RollNo == rollNo)
+                {
+                    student = s;
+                    break;
+                }
+            }
+            return student;
+        }
+        public void AddStudent(Student s)
+        {
+            studentlist.Add(s);
+        }
+
+        public void UpdateStudent(Student s)
+        {
+            foreach (Student student in studentlist)
+            {
+                if (student.RollNo == s.RollNo)
+                {
+                    student.RollNo = s.RollNo;
+                    student.Name = s.Name;
+                    student.Percentage = s.Percentage;
+                    student.CollegeName = s.CollegeName;
+                }
+            }
+        }
+
+        public void DeleteStudent(int rollNo)
+        {
+            foreach (Student student in studentlist)
+            {
+                if (student.RollNo == rollNo)
+                {
+                    studentlist.Remove(student);
+                    break;
+                }
+            }
+        }
+    }
+}
+    
+
